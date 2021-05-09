f3f70db4c01e39a8a312d19f52251c10844ff7fc
rednebula <rednebula01018@gmail.com>
2021-05-09 21:50:02 +0900

    [etc] Add Desk MVVM pattern code blue print.

---
+++ src/Desk/controller/desk.controller.tsx
@@ -0,0 +1,9 @@
+import {DeskView} from '../view/desk.view';
+
+export const DeskController = () => {
+  // TODO: Add event handler, core UI business logic
+  // TODO: Have reference to the view model
+  return (
+    <DeskView />
+  )
+}
---
+++ src/Desk/view-model/desk.view-model.ts
@@ -0,0 +1,3 @@
+export class DeskViewModel {
+  // TODO: Get UI related model information.
+}
